2813c027d97b2f7402c587bbb2165461082775ef
akre8590 <akre8590@gmail.com>
2014-09-02 16:09:15 -0500

    Formulario pantalones

--- public/src/app/pantalones/pantalones.js
+++ public/src/app/pantalones/pantalones.js
@@ -14,37 +14,84 @@ angular.module( 'ngBoilerplate.pantalones', [
         templateUrl: 'pantalones/pantalones.tpl.html'
       }
     },
-    data:{ pageTitle: 'pantalones' }
+    data:{ pageTitle: 'Pantalones' }
   });
 })
 
 .factory('Pantalones', function($resource) {
-    return $resource('/pantalones',{},
+     return $resource('/pantalones/:id',{id: '@id'},
         {
-         add: {method: 'POST'}
+        get: {method: 'GET', isArray: true },
+         add: {method: 'POST'},
+         remove: {method: 'DELETE'},
+         update: {method: 'POST'}
         }
     );
   })
 
 
-.controller( 'pantalonesCtrl', function ( $scope,Pantalones,$location ) {
+.controller( 'pantalonesCtrl', function ( $scope,Pantalones,$location,TableSearch) {
+ $scope.items=[];
+/*table ordered*/
+        var sortingOrder = 'codigo';
+        $scope.sortingOrder = sortingOrder;
+        $scope.headers=[
+            {
+                "class": "fa fa-sort",
+                "text": "Código",
+                "sort_by":"codigo"
+            },
+               {
+                "class": "fa fa-sort",
+                "text": "Estilo",
+                "sort_by":"estilo"
+            },
+               {
+                "class": "fa fa-sort",
+                "text": "Talla",
+                "sort_by":"talla"
+            },
+             {
+                "class": "fa fa-sort",
+                "text": "Nuevos",
+                "sort_by":"nuevos"
+            },
+             {
+                "class": "fa fa-sort",
+                "text": "Usados",
+                "sort_by":"usados"
+            }
+        ];
+   TableSearch.search($scope);
+    var init=function(){
+      Pantalones.get(function(data){
+        $scope.items=data;
+        $scope.search();
+      });
+    };
+  init();
   $scope.guardar=function(){
-    console.log("Estoy guardando");
-    Pantalones.add($scope.pantalon,function(){
-      $location.path("/rentas"); //por mientras
-    });
+      Pantalones.add($scope.prenda,function(item){
+         init();
+         $scope.prenda={};
+      });
   };
+  $scope.cancelar=function(){
+      $scope.prenda={};
+  };
+  $scope.remove=function(item){
+    if(confirm("¿Seguro que desea eliminar este elmento?")){
+      Pantalones.remove({id:item._id},function(){
+         $scope.items.splice($scope.items.indexOf(item), 1);
+         $scope.search();
+      });
+    }
+  };
+  $scope.edit=function(item){
+    $scope.prenda=angular.copy(item);
+  };
+  $scope.title="Pantalones";
 
-
-
-
-
-
-/*$scope.pantalon={};
-$scope.pantalon.codigo=123;
-$scope.pantalon.estilo="cholo";
-$scope.pantalon.talla="Extra Smalville";
-*/
 
 
 
