8c38cf1840d72b2f91c78fc985b2e7d853d9b2d6
Eduardo <olal28@gmail.com>
2014-09-03 21:43:17 -0500

    Cambio de diseño

--- public/src/app/app.js
+++ public/src/app/app.js
@@ -10,6 +10,7 @@ angular.module('ngBoilerplate', [
   'ngBoilerplate.togas',
   'ngBoilerplate.pantalones',
   'ui.router',
+  'ui.bootstrap',
   'ngResource',
   'ngCookies'
 ])
@@ -193,6 +194,30 @@ angular.module('ngBoilerplate', [
         Window.close();
       };
 
+$scope.oneAtATime = true;
+
+  $scope.groups = [
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'Dynamic Group Body - 1'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'Dynamic Group Body - 2'
+    }
+  ];
+
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+
+  $scope.status = {
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
 
 
     });
