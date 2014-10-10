8d3da982aa6dafba8ca5ac7c33467811637a48b6
Eduardo <olal28@gmail.com>
2014-10-10 01:21:24 -0500

    Reporte histórico completo

--- public/src/app/reportes/rentas/rentas.js
+++ public/src/app/reportes/rentas/rentas.js
@@ -17,30 +17,71 @@ angular.module('ngBoilerplate.reportes_rentas', [
       }
     });
   })
-  .controller('reportesRentasCtrl', function reportesRentasCtrl($scope, DatePicker, Prendas, Reportes,Dates,$location) {
+  .factory('ReportesCache', ['$cacheFactory',
+    function($cacheFactory) {
+      return $cacheFactory('reportes');
+    }
+  ])
+  .controller('reportesRentasCtrl', function reportesRentasCtrl($scope, DatePicker, Prendas, Reportes, Dates, $location, ReportesCache) {
+    var type = "reportes";
     $scope.filtrar = function() {
-      $scope.reporte.fecha_inicial.setHours(0,0,0,0).toString();
-      $scope.reporte.fecha_final.setHours(23,59,59,999).toString();
-      Reportes.show({
-        type: 'HISTORICO',
+      $scope.reporte.fecha_inicial.setHours(0, 0, 0, 0);
+      $scope.reporte.fecha_final.setHours(23, 59, 59, 999);
+      ReportesCache.put(type, {
         fecha_inicial: $scope.reporte.fecha_inicial,
-        fecha_final: $scope.reporte.fecha_final
+        fecha_final: $scope.reporte.fecha_final,
+        status: $scope.reporte.status,
+        tipo_fecha: $scope.reporte.tipo_fecha
+      });
+      Reportes.show({
+        type: type,
+        fecha_inicial: $scope.reporte.fecha_inicial,
+        fecha_final: $scope.reporte.fecha_final,
+        status: $scope.reporte.status,
+        tipo_fecha: $scope.reporte.tipo_fecha
       }, function(result) {
-        $scope.items=result;
+        $scope.items = result;
       });
     };
-
-    DatePicker.init($scope);
-    $scope.formatDate=Dates.formatDate;
     if (!$scope.reporte) {
       $scope.reporte = {};
     }
-    $scope.reporte.fecha_inicial = new Date();
-    $scope.reporte.fecha_final = new Date();
-    $scope.getTicket=function(item){
-       $location.path('/rentas/'+item.id);
+    $scope.reporte.tipo_fecha = "fecha_apartado";
+    var historico = ReportesCache.get(type);
+    if (historico) {
+      $scope.reporte.fecha_inicial = historico.fecha_inicial;
+      $scope.reporte.fecha_final = historico.fecha_final;
+      $scope.reporte.status = historico.status;
+      $scope.reporte.tipo_fecha = historico.tipo_fecha;
+      $scope.filtrar();
+    } else {
+      $scope.reporte.fecha_inicial = new Date();
+      $scope.reporte.fecha_final = new Date();
+    }
+    DatePicker.init($scope);
+    $scope.formatDate = Dates.formatDate;
+    $scope.getTicket = function(item) {
+      $location.path('/rentas/' + item.id);
     };
-
+    $scope.getColor=function(item){
+      var currentDate=new Date().setHours(0, 0, 0, 0),
+      fentrega= new Date(item.fecha_entrega).setHours(0, 0, 0, 0),
+      fdevolucion=new Date(item.fecha_devolucion).setHours(0, 0, 0, 0),
+      date=null;
+      console.log(fentrega);
+      console.log(fdevolucion);
+      if(item.status_id===1){
+        if(currentDate>fentrega || currentDate > fdevolucion){
+          return "danger";
+        }
+      }else
+      if(item.status_id===2){
+        if(currentDate > fdevolucion){
+          return "danger";
+        }
+      }
+      return "";
+    };
   })
 
 ;
--- server/reportes.js
+++ server/reportes.js
@@ -8,20 +8,24 @@ module.exports.init = function(app, connection, models, db) {
   app.post('/reportes', function(req, res) {
  var item = req.body;
 var sql=["select tickets.*, sum((prendas.costo_renta*rentas.cantidad)-(prendas.costo_renta*rentas.cantidad)*(descuento/100) ) as total,",
-			"count(rentas.id) as articulos, status_tickets.description as status",
+			"count(rentas.id) as articulos,status_tickets.id as status_id, status_tickets.description as status",
 			"from  tickets",
 			"left join rentas on rentas.ticket_id=tickets.id",
 			"left join prendas on rentas.prenda_id=prendas.id",
 			"left join status_tickets on tickets.status=status_tickets.id",
 			"left join tipo_prendas on tipo_prendas.id=prendas.tipo_prenda",
 			"where tickets.borrado=0 ",
-			"and fecha_devolucion  between ? and ?",
+			"and "+item.tipo_fecha+"  between ? and ?",
+			"and status_tickets.id="+item.status,
 			"group by tickets.id",
-			"order by- fecha_devolucion desc",
+			"order by "+item.tipo_fecha+" desc",
 			];
 		    var fi=item.fecha_inicial,
 			    ff=item.fecha_final,
 				params=[fi,ff];
+			if(!item.status||item.status==="-1"){
+				sql[9]="";
+			}
 			//res.send(sql.join(" ")); return;
     connection.runSqlAll(sql.join(" "),params,
       function(err, result) {
